7bc9228ec7a7c6ebe625bf5ce412ef024e81e64a
zhujuming <you@example.com>
2019-12-11 17:36:41 +0800

    20191210

---
+++ spring-cloud-oauth2/src/main/java/com/zhujuming/vip/config/AuthServerConfig.java
@@ -0,0 +1,160 @@
+package com.zhujuming.vip.config;
+
+import com.alibaba.fastjson.JSON;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Description: 认证模块配置(基于Redis存储JWT)
+ */
+@Configuration
+@EnableAuthorizationServer
+public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    // 资源ID
+    private static final String SOURCE_ID = "majiaxueyuanApp";
+    private static final int ACCESS_TOKEN_TIMER = 60 * 60 * 24;
+    private static final int REFRESH_TOKEN_TIMER = 60 * 60 * 24 * 30;
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+    @Autowired
+    RedisConnectionFactory redisConnectionFactory;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory().withClient("myapp").resourceIds(SOURCE_ID).authorizedGrantTypes("password", "refresh_token").scopes("all").authorities("ADMIN").secret("lxapp").accessTokenValiditySeconds(ACCESS_TOKEN_TIMER)
+                .refreshTokenValiditySeconds(REFRESH_TOKEN_TIMER);
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.accessTokenConverter(accessTokenConverter());
+        endpoints.tokenStore(tokenStore()).authenticationManager(authenticationManager);
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception {
+        // 允许表单认证
+        oauthServer.allowFormAuthenticationForClients();
+    }
+
+    // JWT增强
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter() {
+            /***
+             * 重写增强token方法,用于自定义一些token总需要封装的信息
+             */
+            @Override
+            public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+                String userName = authentication.getUserAuthentication().getName();
+                // 得到用户名，去处理数据库可以拿到当前用户的信息和角色信息（需要传递到服务中用到的信息）
+                final Map<String, Object> additionalInformation = new HashMap<>();
+                // Map假装用户实体
+                Map<String, String> userinfo = new HashMap<>();
+                userinfo.put("id", "1");
+                userinfo.put("username", "LiaoXiang");
+                userinfo.put("qqnum", "438944209");
+                userinfo.put("userFlag", "1");
+                additionalInformation.put("userinfo", JSON.toJSONString(userinfo));
+                ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInformation);
+                OAuth2AccessToken enhancedToken = super.enhance(accessToken, authentication);
+                return enhancedToken;
+            }
+        };
+        // 测试用,资源服务使用相同的字符达到一个对称加密的效果,生产时候使用RSA非对称加密方式
+        accessTokenConverter.setSigningKey("majiaxueyuanSigningKey");
+        return accessTokenConverter;
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        RedisTokenStore tokenStore = new RedisTokenStore(redisConnectionFactory);
+        return tokenStore;
+    }
+//    //资源ID
+//    private static final String SOURCE_ID="zhujumingApp";
+//    private static final int ACCESS_TOKEN_TIMER=60 *60 *24;
+//    private static final int REFRESH_TOKEN_TIMER=60 *60 *24 *30;
+//
+//    //认证管理
+//    @Resource
+//    AuthenticationManager authenticationManager;
+//
+//    //redis连接工具
+//    @Autowired
+//    private RedisConnectionFactory redisConnectionFactory;
+//
+//    @Override
+//    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+//        clients.inMemory().withClient("myapp").resourceIds(SOURCE_ID)
+//                .authorizedGrantTypes("password","refresh_token").scopes("all")
+//                .authorities("ADMIN").secret("sevenapp").accessTokenValiditySeconds(ACCESS_TOKEN_TIMER)
+//                .refreshTokenValiditySeconds(REFRESH_TOKEN_TIMER);
+//    }
+//
+//    @Override
+//    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+//        endpoints.accessTokenConverter(accessTokenConverter());
+//        endpoints.tokenStore(tokenStore()).authenticationManager(authenticationManager);
+//    }
+//
+//    @Override
+//    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+//        //允许表单认证
+//        security.allowFormAuthenticationForClients();
+//    }
+//
+//    //JWT增强
+//    @Bean
+//    public JwtAccessTokenConverter accessTokenConverter(){
+//        JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter(){
+//
+//            //重写增强token方法
+//            @Override
+//            public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+//                String name = authentication.getUserAuthentication().getName();
+//                //得到用户名,去处理数据库可以拿到当前用户的信息和角色信息(需要传递到服务中用到的信息)
+//                HashMap<String, Object> additionalInformationmap = new HashMap<>();
+//                //map假装用户实体
+//                HashMap<Object, Object> userInfo = new HashMap<>();
+//                userInfo.put("id","1");
+//                userInfo.put("username","zhujuming");
+//                userInfo.put("QQ","893371542");
+//                userInfo.put("userFlag","1");
+//                additionalInformationmap.put("userInfo", JSON.toJSONString(userInfo));
+//                ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInformationmap);
+//                OAuth2AccessToken enhancedToken = super.enhance(accessToken,authentication);
+//                return enhancedToken;
+//            }
+//        };
+//        //测试用,资源服务使用相同的字符达到一个对称加密的效果,生产的时候使用RSA非对称加密方式
+//        accessTokenConverter.setSigningKey("zhujumingSigningKey");
+//        return accessTokenConverter;
+//    }
+//
+//    @Bean
+//    public TokenStore tokenStore(){
+//        RedisTokenStore tokenStore = new RedisTokenStore(redisConnectionFactory);
+//        return tokenStore();
+//    }
+}
--- spring-cloud-oauth2/src/main/java/com/zhujuming/vip/config/SecurityConfig.java
+++ spring-cloud-oauth2/src/main/java/com/zhujuming/vip/config/SecurityConfig.java
@@ -1,17 +1,27 @@
 package com.zhujuming.vip.config;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.BasicAuthDefinition;
 import io.swagger.models.auth.In;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.web.cors.CorsUtils;
@@ -23,82 +33,82 @@ import org.springframework.web.cors.CorsUtils;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    //配置这个,以保证在刷新Token时能成功刷新
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-        //配置密码加密方式 BCryptPasswordEncoder,添加用户加密的时候也用这个加密
+    // 请配置这个，以保证在刷新Token时能成功刷新
+    @Autowired
+    public void globalUserDetails(AuthenticationManagerBuilder auth) throws Exception {
+        // 配置密码加密方式 BCryptPasswordEncoder，添加用户加密的时候请也用这个加密
         auth.userDetailsService(userDetailsService()).passwordEncoder(new BCryptPasswordEncoder());
     }
 
+    @Bean
     @Override
     protected UserDetailsService userDetailsService() {
-       //这里是添加两个用户到内存中去,实际中是从下面通过数据判断用户是否存在
+        // 这里是添加两个用户到内存中去，实际中是从#下面去通过数据库判断用户是否存在
         InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        String password = passwordEncoder.encode("zhujuming123");
-        manager.createUser(User.withUsername("zhujuming").password(password).authorities("USER").build());
-        manager.createUser(User.withUsername("seven").password(password).authorities("USER").build());
+        BCryptPasswordEncoder passwordEncode = new BCryptPasswordEncoder();
+        String pwd = passwordEncode.encode("123456");
+        manager.createUser(User.withUsername("user_1").password(pwd).authorities("USER").build());
+        manager.createUser(User.withUsername("user_2").password(pwd).authorities("USER").build());
         return manager;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        //@formatter:off
+        // @formatter:off
         http.requestMatchers().anyRequest().and().authorizeRequests().antMatchers("/oauth/**").permitAll();
+        // @formatter:on
     }
 
-//    package com.foreign.payment.config;
+
+
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+
 //
-//import io.swagger.annotations.Api;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.web.cors.CorsUtils;
+//    //需要放行的URL
+//    private static final String[] AUTH_WHITELIST = {
+//            "/token",
+//            "/v2/api-docs",
+//            "/swagger-resources",
+//            "/swagger-resources/**",
+//            "/configuration/ui",
+//            "/configuration/security",
+//            "/swagger-ui.html",
+//            "/webjars/**",
+//            "/oauth/**"
+//    };
 //
-//    @Slf4j
-//    @Configuration
-//    @EnableWebSecurity
-//    @EnableGlobalMethodSecurity(securedEnabled = true)
-//    @Api("测试")
-//    public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//    //配置这个,以保证在刷新Token时能成功刷新
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 //
-//        /**
-//         * 需要放行的URL
-//         */
-//        private static final String[] AUTH_WHITELIST = {
-////            "/**",
-//                "/token",
-//                // -- swagger ui
-//                "/v2/api-docs",
-//                "/swagger-resources",
-//                "/swagger-resources/**",
-//                "/configuration/ui",
-//                "/configuration/security",
-//                "/swagger-ui.html",
-//                "/webjars/**"
-//        };
-//
-//        // 设置 HTTP 验证规则
-//        @Override
-//        protected void configure(HttpSecurity http) throws Exception {
-//            http.cors().and().csrf().disable()
-//                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                    .and().authorizeRequests()
-//                    .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
-//                    .antMatchers(AUTH_WHITELIST).permitAll();
-//            //.anyRequest().authenticated()  // 所有请求需要身份认证
-//            // .and().exceptionHandling().authenticationEntryPoint(new Http401AuthenticationEntryPoint())
-//            // .and().addFilter(new AuthenticationFilter(authenticationManager())).exceptionHandling().accessDeniedHandler(customAccessDeniedHandler);// 自定义访问失败处理器
-//
-//
-//        }
-//
-//        //  @Autowired
-//        // private CustomAccessDeniedHandler customAccessDeniedHandler;
+//        //配置密码加密方式 BCryptPasswordEncoder,添加用户加密的时候也用这个加密
+//        auth.userDetailsService(userDetailsService()).passwordEncoder(new BCryptPasswordEncoder());
 //    }
+//
+//    @Override
+//    protected UserDetailsService userDetailsService() {
+//        //这里是添加两个用户到内存中去,实际中是从下面通过数据判断用户是否存在
+//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+//        String password = passwordEncoder.encode("zhujuming123");
+//        manager.createUser(User.withUsername("zhujuming").password(password).authorities("USER").build());
+//        manager.createUser(User.withUsername("seven").password(password).authorities("USER").build());
+//        return manager;
+//    }
+//
+//    //设置HTTP验证规则
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.cors().and().csrf().disable()
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and().authorizeRequests()
+//                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+//                .antMatchers(AUTH_WHITELIST).permitAll()
+//                .anyRequest().authenticated(); // 所有请求需要身份认证
+//    }
+
 }
